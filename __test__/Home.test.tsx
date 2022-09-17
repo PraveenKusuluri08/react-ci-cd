73343cb21d85f7adadb64004509c8b0f6f0fc20d
PraveenKusuluri08 <praveen.k@skil.ai>
2022-09-17 15:37:50 +0530

    Animated button component is added and maintained with tests

--- __test__/Home.test.tsx
+++ __test__/Home.test.tsx
@@ -7,6 +7,6 @@ import Home from "../src/components/Home"
 
 it("renders sample h1 tag", () => {
   render(<Home />)
-  const myElement = screen.getByText(/Home!/)
+  const myElement = screen.getByText(/Home/)
   expect(myElement).toBeInTheDocument()
 })
