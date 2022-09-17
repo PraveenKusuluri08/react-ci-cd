7b95503cadbdac76ab0cab118f02708402a4d993
PraveenKusuluri08 <praveen.k@skil.ai>
2022-09-17 12:50:02 +0530

    Basic template and sample test

---
+++ __test__/Home.test.tsx
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from "@testing-library/react"
+import React from "react"
+import Home from "../src/components/Home"
+
+it("renders sample h1 tag", () => {
+  render(<Home />)
+  const myElement = screen.getByText(/Home!/)
+  expect(myElement).toBeInTheDocument()
+})
---
+++ jest.config.js
@@ -0,0 +1,19 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      tsconfig: "./tsconfig.jest.json",
+    },
+  },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  setupFilesAfterEnv: ["./src/jest.setup.ts"],
+  transform: {},
+}
